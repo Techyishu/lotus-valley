39eb2b671c698a3532ab30657e95219b1c72968a
Techyishu <shashanksingh67567@gmail.com>
2025-12-16 12:33:07 +0530

    ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'anthem-public-school/website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8bb83d64c3b0684ae7dbf292e7d1a277360302cf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'anthem-public-school/website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8bb83d64c3b0684ae7dbf292e7d1a277360302cf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
